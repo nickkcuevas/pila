8182c4e5bf19069df58a755de4fee27aa668c016
ncuevas <nicolas.cuevas@greencodesoftware.com>
2018-05-15 10:50:15 -0300

    add tests for pila

--- pila.c
+++ pila.c
@@ -1,6 +1,7 @@
 #include "pila.h"
 #include <stdlib.h>
-
+size_t TAMANIO = 100;
+size_t REDIMENSION = 3;
 /* Definición del struct pila proporcionado por la cátedra.
  */
 struct pila {
@@ -13,4 +14,86 @@ struct pila {
  *                    PRIMITIVAS DE LA PILA
  * *****************************************************************/
 
-// ...
+
+// Crea una pila.
+// Post: devuelve una nueva pila vacía.
+pila_t* pila_crear(void){
+    pila_t* pila = malloc(sizeof(pila_t));
+    if (pila == NULL) return NULL;
+    pila->cantidad = 0;
+    pila->capacidad = TAMANIO; // refactor por VARIABLE GLOBAL
+    pila->datos = (void*)malloc(sizeof(void*) * pila->capacidad);
+    if (pila->datos == NULL){
+        free(pila);
+        return NULL;
+    }
+    return pila;
+}
+
+
+// Destruye la pila.
+// Pre: la pila fue creada.
+// Post: se eliminaron todos los elementos de la pila.
+void pila_destruir(pila_t *pila){
+    free(pila->datos);
+    free(pila);
+}
+
+// Devuelve verdadero o falso, según si la pila tiene o no elementos apilados.
+// Pre: la pila fue creada.
+bool pila_esta_vacia(const pila_t *pila){
+    return pila->cantidad == 0;
+}
+
+// Agrega un nuevo elemento a la pila. Devuelve falso en caso de error.
+// Pre: la pila fue creada.
+// Post: se agregó un nuevo elemento a la pila, valor es el nuevo tope.
+bool pila_apilar(pila_t *pila, void* valor){
+    if (pila->cantidad == pila->capacidad){
+        void** datos_nuevo = realloc(pila->datos, REDIMENSION * sizeof(void*));
+        if (!datos_nuevo){
+            free(datos_nuevo);
+            return false;
+        }
+        pila->datos = datos_nuevo;
+    }
+    pila->datos[pila->cantidad] = valor;
+    pila->cantidad++;
+    return true;
+}
+
+// Obtiene el valor del tope de la pila. Si la pila tiene elementos,
+// se devuelve el valor del tope. Si está vacía devuelve NULL.
+// Pre: la pila fue creada.
+// Post: se devolvió el valor del tope de la pila, cuando la pila no está
+// vacía, NULL en caso contrario.
+void* pila_ver_tope(const pila_t *pila){
+    if (pila_esta_vacia(pila)) return NULL;
+    void* tope = &pila->datos[pila->cantidad - 1];
+    return tope;
+}
+
+// Saca el elemento tope de la pila. Si la pila tiene elementos, se quita el
+// tope de la pila, y se devuelve ese valor. Si la pila está vacía, devuelve
+// NULL.
+// Pre: la pila fue creada.
+// Post: si la pila no estaba vacía, se devuelve el valor del tope anterior
+// y la pila contiene un elemento menos.
+void* pila_desapilar(pila_t *pila){
+    if (pila_esta_vacia(pila)) return NULL;
+    void* tope = pila_ver_tope(pila);
+    pila->cantidad--;
+    // redimension
+    if (pila->capacidad / 3 < REDIMENSION){
+        void** datos_nuevo = realloc(pila->datos, pila->capacidad / REDIMENSION * sizeof(void*));
+        if (!datos_nuevo){
+            // duda???
+            free(datos_nuevo);
+        }
+        else{
+            pila->datos = datos_nuevo;
+            pila->capacidad = pila->capacidad / REDIMENSION;
+        }
+    }
+    return tope;
+}
--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,14 +1,55 @@
 #include "pila.h"
 #include "testing.h"
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
 /* ******************************************************************
  *                   PRUEBAS UNITARIAS ALUMNO
  * *****************************************************************/
 
-void pruebas_pila_alumno() {
-    pila_t* ejemplo = NULL;
 
-    print_test("Puntero inicializado a NULL", ejemplo == NULL);
+/* Pruebas para una pila vacia */
+void pruebas_pila_vacia() {
+    printf("INICIO DE PRUEBAS CON PILA VACIA \n");
+
+    /* Declaro las variables a utilizar*/
+    pila_t* pila = pila_crear();
+
+    /* Inicio de pruebas */
+    print_test("crear pila vacia inicialmente vacia es true", pila_esta_vacia(pila) == true);
+    print_test("obtener tope de pila devuelve NULL es true", pila_ver_tope(pila) == NULL);
+    print_test("pila desapilar devuelve NULL es true", pila_desapilar(pila) == NULL);
+
+    pila_destruir(pila);
+    print_test("la pila fue destruida", true);
+}
+
+
+/* Pruebas para una pila que contiene elementos */
+void pruebas_pila_con_elementos() {
+    printf("INICIO DE PRUEBAS CON CON PILA AGREGANDO ELEMENTOS \n");
+
+    /* Declaro las variables a utilizar*/
+    pila_t* pila = pila_crear();
+    size_t tam = 10;
+    int** datos = malloc(tam * sizeof(int));
+    for (int i=0; i < tam; i++){
+        *datos[i] = i;
+        pila_apilar(pila, &datos[i]);
+    }
+
+    /* Inicio de pruebas */
+    print_test("obtener tope de pila devuelve NULL es true", *((int*)pila_ver_tope(pila)) == 9);
+    
+    pila_destruir(pila);
+    print_test("la pila fue destruida", true);
+}
+
+
+
+void pruebas_pila_alumno() {
+    pruebas_pila_vacia();
+    pruebas_pila_con_elementos();
 }
